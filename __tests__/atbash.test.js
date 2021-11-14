cdfc69e5e9d82a861843f0795d48cf301e4f7890
ML6503 <ml6503@gmail.com>
2021-11-14 16:07:42 +0200

    feat: add more test for Etbash encode

--- __tests__/atbash.test.js
+++ __tests__/atbash.test.js
@@ -5,6 +5,10 @@ test("Atbash Cipher on enter 'bla_' will decode as 'yoz_' ", () => {
   expect(atbashCipher("bla_")).toEqual("yoz_");
 });
 
+test("Atbash Cipher will encode 'R olev Zgyzhs!' as 'I love Atbash!'", () => {
+  expect(atbashCipher("R olev Zgyzhs!")).toEqual("I love Atbash!");
+});
+
 test("Atbash Cipher if not string provided as argument will throw an error", () => {
   expect(() => atbashCipher(123)).toThrow(argError);
 });
